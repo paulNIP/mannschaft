84cd26bcd9f078141a4da22dacb129669974d7df
PaulOtim <paulalex.otim@outlook.com>
2024-06-28 16:29:24 +0300

    Completed Phase 1 Reading Pdf Report

---
+++ src/main/java/com/opolos/mannschaft/services/PdfProcessingTask.java
@@ -0,0 +1,137 @@
+package com.opolos.mannschaft.services;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+
+@Component
+public class PdfProcessingTask {
+
+    @Value("${pdf.folder.path}")
+    private String folderPath;
+
+    static String[] lines;
+
+    @Scheduled(cron = "0 * * * * ?") // Every Minute
+    public void processPdfFiles() {
+        File folder = new File(folderPath);
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".pdf"));
+
+            if (files != null) {
+                for (File file : files) {
+                    try {
+                       
+                        processPdf(file);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }else{
+            System.out.println("Folder Soesnt exist Yooooooooo");
+        }
+    }
+
+    private void processPdf(File file) throws IOException {
+        
+
+        try (PDDocument document = PDDocument.load(file)) {
+            
+
+            String filename =  file.getName();
+            String filepath =  file.getPath();
+
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            String text = pdfStripper.getText(document);
+            lines = text.split(System.getProperty("line.separator"));
+            // System.out.println("Text in PDF: " + text);
+            // Further processing of the text
+
+            String clientId =lines[getLineNumberWithFilter("Auftraggeber (Kunde):")].split(":  ")[1];
+
+            // client Details
+            String client=lines[2];
+            String street=lines[3];
+            String address=lines[4];
+
+            //application type,identification number,test_according_to
+
+            String application_type=lines[getLineNumberWithFilter("Geräteart: ")].split(":  ")[1].split(" ")[0];
+            String identification_number=lines[getLineNumberWithFilter("Ident-. Nr.: ")].split(": ")[1];
+            String test_according_to=lines[14];
+
+            
+            //manufucturer,department,measuring length,  profile
+            String manufucturer = lines[getLineNumberWithFilter("Hersteller: ")].split(":  ")[1];
+            String department = lines[getLineNumberWithFilter("Abteilung: ")].split(":  ")[1];
+            String measuring_length = lines[getLineNumberWithFilter("Schutzleiterlänge (m): ")].split(":  ")[1];
+            String profile = lines[getLineNumberWithFilter("Messprofil: ")].split(":  ")[1];
+
+            //manufucturer,department,measuring length,  profile,type
+            String type_number = lines[getLineNumberWithFilter("Typ:")];
+            String serial_number = lines[getLineNumberWithFilter("Seriennummer:")];
+            String device_type = lines[getLineNumberWithFilter("Gerätetyp: ")];
+            String cross_section = lines[getLineNumberWithFilter("Querschnitt (qmm): ")];
+            String test_date = lines[getLineNumberWithFilter("Prüfdatum: ")];
+
+            String last = test_date.substring(test_date.lastIndexOf(' ') + 1);
+
+            String serialNumber=serial_number.substring(serial_number.lastIndexOf(':') + 1);
+            String deviceType=device_type.substring(device_type.lastIndexOf(':') + 1);
+            String crossSection=cross_section.substring(cross_section.lastIndexOf(':') + 1);
+
+            String testDate=dateConvert(last);
+            String next = lines[getLineNumberWithFilter("Nächster Prüftermin: ")];
+            String next_test_date = dateConvert(next.substring(next.lastIndexOf(' ') + 1));
+
+            String num =type_number.substring(17);
+            String examiner = lines[getLineNumberWithFilter("Prüfer: ")].split(": ")[1].split(" ")[0];
+        }
+    }
+
+
+    // searches all lines for first line index containing `filter`
+    static int getLineNumberWithFilter(String filter) {
+        int n = 0;
+        for(String line : lines) {
+            if(line.indexOf(filter) != -1) {
+                return n;
+            }
+            n++;
+        }
+        return -1;
+    }
+
+    // returns a full String line by number n
+    static String getLine(int n) {
+        return lines[n];
+    }
+
+    static String dateConvert(String inputDateString){
+
+        // Define the input and output date formats
+        DateTimeFormatter inputFormat = DateTimeFormat.forPattern("dd.MM.yyyy");
+        DateTimeFormatter outputFormat = DateTimeFormat.forPattern("yyyy-MM-dd");
+        
+        // Parse the input date string to a DateTime object
+        DateTime dateTime = inputFormat.parseDateTime(inputDateString);
+        
+        // Format the DateTime object to the desired output format
+        String outputDateString = outputFormat.print(dateTime);
+
+        return outputDateString;
+    }
+    
+}
---
+++ src/main/java/com/opolos/mannschaft/services/PdfService.java
@@ -0,0 +1,19 @@
+package com.opolos.mannschaft.services;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.IOException;
+
+@Service
+public class PdfService {
+
+    public String readPdf(File file) throws IOException {
+        try (PDDocument document = PDDocument.load(file)) {
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+            return pdfStripper.getText(document);
+        }
+    }
+}
