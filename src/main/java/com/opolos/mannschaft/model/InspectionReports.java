1af380d4cacf9ef15126a6ed03293366b85d17b8
PaulOtim <paulalex.otim@outlook.com>
2024-07-04 15:42:56 +0300

    shgshjhshshhs

--- src/main/java/com/opolos/mannschaft/model/InspectionReports.java
+++ src/main/java/com/opolos/mannschaft/model/InspectionReports.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class InspectionReports {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
 
 
