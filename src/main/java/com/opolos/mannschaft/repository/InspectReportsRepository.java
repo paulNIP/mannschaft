f799c0e561dbee41fff95586c27aeee60a916693
PaulOtim <paulalex.otim@outlook.com>
2024-07-17 20:20:20 +0300

    MMssjjdjdj

--- src/main/java/com/opolos/mannschaft/repository/InspectReportsRepository.java
+++ src/main/java/com/opolos/mannschaft/repository/InspectReportsRepository.java
@@ -1,11 +1,7 @@
 package com.opolos.mannschaft.repository;
 
-import java.util.List;
 import java.util.Optional;
-
-import org.springframework.boot.autoconfigure.amqp.RabbitConnectionDetails.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
